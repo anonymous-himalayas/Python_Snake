b85dce2e395a2a2b8ec021dbbd12ce40f85fd019
Himal <hpmalik@uci.edu>
2024-02-27 13:48:16 -0800

    Added rough skeleton for how program should look

--- main_snake.py
+++ main_snake.py
@@ -1,3 +1,22 @@
 import tkinter
 import random
 
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collision():
+    pass
+
+def game_over():
+    pass
+
+
