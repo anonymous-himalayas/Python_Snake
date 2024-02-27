8872538d8c7eb3ea14970795b1a134d835ef7020
Himal <hpmalik@uci.edu>
2024-02-27 13:52:25 -0800

    Added global constants for game, possible to change

--- main_snake.py
+++ main_snake.py
@@ -1,6 +1,18 @@
 import tkinter
 import random
 
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 40
+SPACE_SIZE = 40
+BODY_SPACING = 3
+SNAKE_COLOR = '#00FF00'
+FOOD_COLOR = 'FF0000'
+BACKGROUND = '000000'
+
+
+
+
 class Snake:
     pass
 
@@ -19,4 +31,6 @@ def check_collision():
 def game_over():
     pass
 
+def main():
+    pass
 
