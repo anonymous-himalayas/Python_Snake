961dac351496e3701a939eeed600a51b3dc8b0ba
Himal <hpmalik@uci.edu>
2024-02-27 14:52:38 -0800

    Snake is now moving in one direction and has limited tail

--- main_snake.py
+++ main_snake.py
@@ -3,7 +3,7 @@ import random
 
 GAME_WIDTH = 700
 GAME_HEIGHT = 700
-SPEED = 40
+SPEED = 50
 SPACE_SIZE = 50
 BODY_SPACING = 3
 SNAKE_COLOR = '#00FF00'
@@ -36,6 +36,9 @@ class Snake:
     def insert_square(self, index, value):
         self.squares.insert(index, value)
 
+    def delete_square(self,value):
+        del self.squares[value]
+
 class Food:
     def __init__(self, canvas):
         x = random.randint(0,int(GAME_WIDTH/SPACE_SIZE - 1) * SPACE_SIZE)
@@ -44,7 +47,7 @@ class Food:
         canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill = FOOD_COLOR, tag= 'food')
 
 
-def next_turn(snake, food, direction):
+def next_turn(snake, food):
     x,y = snake.get_coordinates()[0]
 
     if direction == 'up':
@@ -58,9 +61,14 @@ def next_turn(snake, food, direction):
 
     snake.insert_coordinates(0, (x,y))
 
-    square = tkinter.canvas.create_rectangle(x,y,x+SPACE_SIZE,y + SPACE_SIZE, fill=SNAKE_COLOR, )
+    square = canvas.create_rectangle(x,y,x+SPACE_SIZE,y + SPACE_SIZE, fill=SNAKE_COLOR, )
     snake.insert_square(0, square)
-    tkinter.window.after(SPEED, next_turn, snake, food)
+
+    del snake.coordinates[-1]
+    canvas.delete(snake.get_squares()[-1])
+    snake.delete_square(-1)
+
+    window.after(SPEED, next_turn, snake, food)
 
 def change_direction(new_direction):
     pass
@@ -71,36 +79,34 @@ def check_collision():
 def game_over():
     pass
 
-def run():
-    window = tkinter.Tk()
-    window.title('Snake Game')
-    window.resizable(False, False)
 
-    score = 0
-    direction = 'down'
-    score_label = tkinter.Label(window, text="Score: {}".format(score), font=('consolas', 40))
-    score_label.pack()
+window = tkinter.Tk()
+window.title('Snake Game')
+window.resizable(False, False)
 
-    canvas = tkinter.Canvas(window, bg = BACKGROUND, width=GAME_WIDTH, height= GAME_HEIGHT)
-    canvas.pack()
+score = 0
+direction = 'down'
+score_label = tkinter.Label(window, text="Score: {}".format(score), font=('consolas', 40))
+score_label.pack()
 
-    window.update()
-    window_width = window.winfo_width()
-    window_height = window.winfo_height()
-    screen_width = window.winfo_screenwidth()
-    screen_height = window.winfo_screenheight()
+canvas = tkinter.Canvas(window, bg = BACKGROUND, width=GAME_WIDTH, height= GAME_HEIGHT)
+canvas.pack()
 
-    x_axis = int((screen_width / 2) - (window_width / 2))
-    y_axis = int((screen_height / 2) - (window_height / 2))
+window.update()
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
 
-    window.geometry(f"{window_width}x{window_height}+{x_axis}+{y_axis}")
+x_axis = int((screen_width / 2) - (window_width / 2))
+y_axis = int((screen_height / 2) - (window_height / 2))
 
-    snake = Snake(canvas)
-    food = Food(canvas)
-    next_turn(snake, food, direction)
+window.geometry(f"{window_width}x{window_height}+{x_axis}+{y_axis}")
 
-    window.mainloop()
+snake = Snake(canvas)
+food = Food(canvas)
+next_turn(snake, food)
+
+window.mainloop()
 
 
-if __name__ == '__main__':
-    run()
